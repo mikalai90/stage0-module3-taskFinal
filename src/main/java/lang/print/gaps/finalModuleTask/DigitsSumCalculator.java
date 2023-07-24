7158ea4dc6f335f84445d70aa1fe7c87ba5b0457
Mikalai Alkhimenka <alkhimenkamikalai90@gmail.com>
2023-07-24 19:49:11 +0200

    Solved

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int a, b, c, d;
+        d = number % 10;
+        c = (number / 10) % 10;
+        b = (number / 100) % 10;
+        a = (number / 1000) % 10;
+        System.out.println(a + b + c + d);
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int a, b, c;
+        c = number % 10;
+        b = (number / 10) % 10;
+        a = (number / 100) % 10;
+        System.out.println(c + "" + b + "" + a);
     }
 }
